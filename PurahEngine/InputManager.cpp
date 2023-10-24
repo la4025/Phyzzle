b4ed838fa6e2b6bfc3b04f65cacee6a90a90a94c
nadong <ehdgns4025@gmail.com>
2023-10-24 19:19:47 +0900

    20231024 하원 전 커밋 / CMake 다루느라 하루가 다갔다.

--- PurahEngine/InputManager.cpp
+++ PurahEngine/InputManager.cpp
@@ -19,16 +19,19 @@ void PurahEngine::InputManager::Update()
 	}
 }
 
+// 키를 방금 눌렀는가
 bool PurahEngine::InputManager::IsKeyDown(int keycode)
 {
 	return (PrevKeyState[keycode] == 0) && (NowKeyState[keycode] & 0x8001);
 }
 
+// 키를 눌리고 있는가
 bool PurahEngine::InputManager::IsKeyPressed(int keycode)
 {
 	return (PrevKeyState[keycode] & 0x8001) && (NowKeyState[keycode] & 0x8001);
 }
 
+// 키를 뗐는가
 bool PurahEngine::InputManager::IsKeyUp(int keycode)
 {
 	return (PrevKeyState[keycode] & 0x8001) && (NowKeyState[keycode] == 0);
