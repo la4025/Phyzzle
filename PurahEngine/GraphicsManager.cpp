78ac15e385f4f3d70f3bf55980dbc19acd5d6515
nadong <ehdgns4025@gmail.com>
2023-11-14 20:52:42 +0900

    20231114 하원 전 커밋 / 그래픽엔진과 연동 완료. 부가적으로 할 일들 생각할 것. 최우선으로 Renderer 컴포넌트 만들 것.

--- PurahEngine/GraphicsManager.cpp
+++ PurahEngine/GraphicsManager.cpp
@@ -1,4 +1,11 @@
 #include "GraphicsManager.h"
+
+#include "GameObject.h"
+#include "Transform.h"
+#include "SceneManager.h"
+#include "InputManager.h"
+#include <Eigen/Dense>
+
 #include <cassert>
 
 void PurahEngine::GraphicsManager::Initialize(HWND hWnd)
@@ -17,14 +24,18 @@ void PurahEngine::GraphicsManager::Initialize(HWND hWnd)
 	}
 	renderer = createZeldaRenderer();
 	renderer->Initialize(1920, 1080, true, hWnd, false, 1000.0f, 1.0f);
-	renderer->CreateResources();
+	renderer->CreateBasicResources();
 }
 
 void PurahEngine::GraphicsManager::Run()
 {
 	// 그래픽스 연결 테스트
+	static PurahEngine::GameObject* testObject = PurahEngine::SceneManager::GetInstance().CreateGameObject(L"testObject");
+
 	static bool run = true;
-	static ResourceID textureID = ID_NULL;
+	static TextureID textureID = TextureID::ID_NULL;
+	static ModelID modelID = ModelID::ID_NULL;
+	static CameraID cameraID = CameraID::ID_NULL;
 	Eigen::Matrix4f test = Eigen::Matrix4f::Identity();
 	Eigen::Matrix4f camera;
 	camera.normalized();
@@ -41,18 +52,49 @@ void PurahEngine::GraphicsManager::Run()
 		renderer->SetMainCamera(resourceID);
 		renderer->UpdateCamera(resourceID, camera, 3.141592654f / 4.0f, 1.0f, 1000.0f);
 		textureID = renderer->CreateTexture(L"scd.jpg");
-		if (textureID == ID_NULL)
+		if (textureID == TextureID::ID_NULL)
 		{
 			assert(0);
 		}
 	}
-	renderer->BeginDraw();
-	float angle = 3.14f / 4.0f;  // 90도를 라디안으로 표현
+	renderer->BeginDraw(0.0f);
+	float angle = 3.14f / 18.0f;  // 90도를 라디안으로 표현
 	Eigen::Matrix3f rotation_matrix;
 	rotation_matrix = Eigen::AngleAxisf(angle, Eigen::Vector3f::UnitY());
 	test.block<3, 3>(0, 0) = rotation_matrix;
 
-	renderer->DrawCube(test, textureID, false);
+	if (PurahEngine::InputManager::Getinstance().IsKeyPressed('Q') == true)
+	{
+		testObject->GetComponent<Transform>()->SetLocalRotation(testObject->GetComponent<Transform>()->GetLocalRotation() * 
+																Eigen::Quaternionf(Eigen::AngleAxisf(angle, Eigen::Vector3f::UnitY())));
+	}
+
+	if (PurahEngine::InputManager::Getinstance().IsKeyPressed('E') == true)
+	{
+		testObject->GetComponent<Transform>()->SetLocalRotation(testObject->GetComponent<Transform>()->GetLocalRotation() *
+			Eigen::Quaternionf(Eigen::AngleAxisf(-angle, Eigen::Vector3f::UnitY())));
+	}
+
+	if (PurahEngine::InputManager::Getinstance().IsKeyPressed(VK_UP) == true)
+	{
+		testObject->GetComponent<Transform>()->SetLocalScale(testObject->GetComponent<Transform>()->GetLocalScale().cwiseProduct(Eigen::Vector3f(1.1f, 1.1f, 1.1f)));
+	}
+
+	if (PurahEngine::InputManager::Getinstance().IsKeyPressed(VK_DOWN) == true)
+	{
+		testObject->GetComponent<Transform>()->SetLocalScale(testObject->GetComponent<Transform>()->GetLocalScale().cwiseQuotient(Eigen::Vector3f(1.1f, 1.1f, 1.1f)));
+	}
+
+	if (PurahEngine::InputManager::Getinstance().IsKeyPressed('W') == true)
+	{
+		testObject->GetComponent<Transform>()->SetLocalPosition(testObject->GetComponent<Transform>()->GetLocalPosition() + Eigen::Vector3f(0.0f, 1.0f, 0.0f));
+	}
+	if (PurahEngine::InputManager::Getinstance().IsKeyPressed('S') == true)
+	{
+		testObject->GetComponent<Transform>()->SetLocalPosition(testObject->GetComponent<Transform>()->GetLocalPosition() + Eigen::Vector3f(0.0f, -1.0f, 0.0f));
+	}
+
+	renderer->DrawCube(testObject->GetComponent<Transform>()->GetLocalMatrix(), textureID, false, 0, 1, 0, 1);
 	renderer->EndDraw();
 }
 
