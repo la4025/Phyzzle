762fbc92b88ad597340f768b339f865f96e997a4
nadong <ehdgns4025@gmail.com>
2023-11-23 11:11:38 +0900

    마스터 병합 전 커밋

--- PurahEngine/Camera.cpp
+++ PurahEngine/Camera.cpp
@@ -1,1 +1,17 @@
 #include "Camera.h"
+
+PurahEngine::Camera::Camera()
+	: camera(Eigen::Matrix4f::Identity())
+{
+
+}
+
+PurahEngine::Camera::~Camera()
+{
+
+}
+
+void PurahEngine::Camera::SetCameraPosition()
+{
+
+}
--- PurahEngine/Camera.h
+++ PurahEngine/Camera.h
@@ -1,14 +1,20 @@
 #pragma once
 #include "Component.h"
-
+#include "IZeldaRenderer.h"
 namespace PurahEngine
 {
 	class Camera : public Component
 	{
 	public:
+		Camera();
+		virtual ~Camera();
+
+		CameraID cameraID = CameraID::ID_NULL;
+
+		void SetCameraPosition();
 
 	private:
-
+		Eigen::Matrix4f camera;
 	};
 }
 
