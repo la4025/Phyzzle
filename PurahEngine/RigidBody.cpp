5b9f12c32b766ada5f536129d5b4ec48f8e50d78
buddas-bible <kh951220@gmail.com>
2023-12-26 11:51:06 +0900

    연결중

--- PurahEngine/RigidBody.cpp
+++ PurahEngine/RigidBody.cpp
@@ -1,1 +1,156 @@
 #include "RigidBody.h"
+
+#include "PhysicsSystem.h"
+
+namespace PurahEngine
+{
+	RigidBody::RigidBody() : body()
+	{
+
+	}
+
+	RigidBody::~RigidBody()
+	{
+
+	}
+
+	void RigidBody::Awake()
+	{
+		// body = PhysicsSystem::GetInstance().CreateRigidBody();
+	}
+
+	Eigen::Vector3f RigidBody::GetPosition() const
+	{
+		return body->GetPosition();
+	}
+
+	void RigidBody::SetPosition(const Eigen::Vector3f& _position) noexcept
+	{
+		body->SetPosition(_position);
+	}
+
+	Eigen::Quaternionf RigidBody::GetQuaternion() const noexcept
+	{
+		return body->GetQuaternion();
+	}
+
+	void RigidBody::SetQuaternion(const Eigen::Quaternionf& _quaternion) noexcept
+	{
+		body->SetQuaternion(_quaternion);
+	}
+
+	void RigidBody::WakeUp() noexcept
+	{
+		body->WakeUp();
+	}
+
+	bool RigidBody::IsSleeping() const noexcept
+	{
+		return body->IsSleeping();
+	}
+
+	ZonaiPhysics::DynamicLocks RigidBody::GetDynamicLockFlags() const noexcept
+	{
+		return body->GetDynamicLockFlags();
+	}
+
+	void RigidBody::SetDynamicLockFlag(ZonaiPhysics::DynamicLock flag, bool value) noexcept
+	{
+		body->SetDynamicLockFlag(flag, value);
+	}
+
+	void RigidBody::SetDynamicLockFlags(ZonaiPhysics::DynamicLocks flags) noexcept
+	{
+		body->SetDynamicLockFlags(flags);
+	}
+
+	float RigidBody::GetMass() const noexcept
+	{
+		return  body->GetMass();
+	}
+
+	void RigidBody::SetMass(float _mass) noexcept
+	{
+		body->SetMass(_mass);
+	}
+
+	float RigidBody::GetLinearDamping() const noexcept
+	{
+		return body->GetLinearDamping();
+	}
+
+	void RigidBody::SetLinearDamping(float _damping) noexcept
+	{
+		body->SetLinearDamping(_damping);
+	}
+
+	float RigidBody::GetAngularDamping() const noexcept
+	{
+		return body->GetAngularDamping();
+	}
+
+	void RigidBody::SetAngularDamping(float _damping) noexcept
+	{
+		body->SetAngularDamping(_damping);
+	}
+
+	Eigen::Vector3f RigidBody::GetLinearVelocity() const noexcept
+	{
+		return body->GetLinearVelocity();
+	}
+
+	void RigidBody::SetLinearVelocity(const Eigen::Vector3f& _velocity) noexcept
+	{
+		body->SetLinearVelocity(_velocity);
+	}
+
+	Eigen::Vector3f RigidBody::GetAngularVelocity() const noexcept
+	{
+		return body->GetAngularVelocity();
+	}
+
+	void RigidBody::SetAngularVelocity(const Eigen::Vector3f& _velocity) noexcept
+	{
+		body->SetAngularVelocity(_velocity);
+	}
+
+	float RigidBody::GetMaxLinearVelocity() const noexcept
+	{
+		return body->GetMaxLinearVelocity();
+	}
+
+	void RigidBody::SetMaxLinearVelocity(const float& _velo) noexcept
+	{
+		body->SetMaxLinearVelocity(_velo);
+	}
+
+	float RigidBody::GetMaxAngularVelocity() const noexcept
+	{
+		return body->GetMaxAngularVelocity();
+	}
+
+	void RigidBody::SetMaxAngularVelocity(const float& _velo) noexcept
+	{
+		body->SetMaxAngularVelocity(_velo);
+	}
+
+	void RigidBody::AddForce(const Eigen::Vector3f& _force, ZonaiPhysics::ForceType _type) noexcept
+	{
+		body->AddForce(_force, _type);
+	}
+
+	void RigidBody::ClearForce() noexcept
+	{
+		body->ClearForce();
+	}
+
+	void RigidBody::AddTorque(const Eigen::Vector3f& _torque, ZonaiPhysics::ForceType _type) noexcept
+	{
+		body->AddTorque(_torque, _type);
+	}
+
+	void RigidBody::ClearTorque() noexcept
+	{
+		body->ClearTorque();
+	}
+}
--- PurahEngine/RigidBody.h
+++ PurahEngine/RigidBody.h
@@ -1,19 +1,118 @@
 #pragma once
 /// ZonaiPhysics의 RigidBody를 래핑해준다.
 #include "Component.h"
+#include <Eigen/Dense>
+#include "ZnRigidBody.h"
+
+namespace ZonaiPhysics
+{
+	class ZnRigidBody;
+}
 
 namespace PurahEngine
 {
 	class RigidBody : public Component
 	{
 	public:
+		RigidBody();
+		~RigidBody() override;
+
+	public:
+		void Awake() override;
+
+	public:
+		/**
+		위치
+		*/
+		Eigen::Vector3f	GetPosition() const noexcept;
+		void		SetPosition(const Eigen::Vector3f& _position) noexcept;
+
+		/**
+		회전
+		*/
+		Eigen::Quaternionf	GetQuaternion() const noexcept;
+		void		SetQuaternion(const Eigen::Quaternionf& _quaternion) noexcept;
+
+		/**
+		수면 상태의 강체를 깨움
+		*/
+		void		WakeUp() noexcept;
+
+		/**
+		수면 여부를 반환함
+		*/
+		bool		IsSleeping() const noexcept;
+
+		/**
+		강체의 움직임을 제한하는 플래그
+		*/
+		ZonaiPhysics::DynamicLocks GetDynamicLockFlags() const noexcept;
+		void		SetDynamicLockFlag(ZonaiPhysics::DynamicLock flag, bool) noexcept;
+		void		SetDynamicLockFlags(ZonaiPhysics::DynamicLocks flags) noexcept;
+
+	public:
+		/**
+		질량
+
+		물체의 질량.
+		0이면 무한한 질량으로 취급함.
+		*/
+		float		GetMass() const noexcept;
+		void		SetMass(float) noexcept;
+
+		/**
+		선형 감쇠 계수
+
+		저항이나 여러가지 요소에 의해 물체가 서서히 정지함.
+		*/
+		float		GetLinearDamping() const noexcept;
+		void		SetLinearDamping(float _damping) noexcept;
+
+		/**
+		회전 감쇠 계수
+
+		저항이나 여러가지 요소에 의해 물체가 서서히 정지함
+		*/
+		float		GetAngularDamping() const noexcept;
+		void		SetAngularDamping(float _damping) noexcept;
+
+		/**
+		선속도
+		*/
+		Eigen::Vector3f	GetLinearVelocity() const noexcept;
+		void		SetLinearVelocity(const Eigen::Vector3f& _velocity) noexcept;
+
+		/**
+		각속도
+		*/
+		Eigen::Vector3f	GetAngularVelocity() const noexcept;
+		void		SetAngularVelocity(const Eigen::Vector3f& _velocity) noexcept;
+
+		/**
+		선속도 제한
+		*/
+		float		GetMaxLinearVelocity() const noexcept;
+		void		SetMaxLinearVelocity(const float&) noexcept;
+
+		/**
+		각속도 제한
+		*/
+		float		GetMaxAngularVelocity() const noexcept;
+		void		SetMaxAngularVelocity(const float&) noexcept;
+
+		/**
+		물체에 힘을 가하거나 지움
+		*/
+		void		AddForce(const Eigen::Vector3f& _force, ZonaiPhysics::ForceType _type = ZonaiPhysics::ForceType::Force) noexcept;
+		void		ClearForce() noexcept;
+
+		/**
+		물체에 토크를 가하거나 지움
+		*/
+		void		AddTorque(const Eigen::Vector3f& _torque, ZonaiPhysics::ForceType _type = ZonaiPhysics::ForceType::Force) noexcept;
+		void		ClearTorque() noexcept;
 
 	private:
-
+		ZonaiPhysics::ZnRigidBody* body;
 	};
 }
-
-class RigidBody
-{
-};
-
