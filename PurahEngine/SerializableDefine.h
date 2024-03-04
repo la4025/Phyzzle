d1ca0c958080db56d3454fb48dc0651c2cafe9fa
nadong <ehdgns4025@gmail.com>
2024-03-04 14:57:41 +0900

    매크로함수 PREDESERIALIZE_VECTOR 제작

--- PurahEngine/SerializableDefine.h
+++ PurahEngine/SerializableDefine.h
@@ -33,6 +33,15 @@
 	value = Eigen::Quaternionf(jsonData[#value]["w"], jsonData[#value]["x"], jsonData[#value]["y"], jsonData[#value]["z"]); \
 }
 
+/// 범용적으로 벡터를 담을 매크로함수 만들기가 좀 어렵다. 더 고민해보는중.
+#define PREDESERIALIZE_VECTOR(value)\
+{\
+	for(int i = 0; i < jsonData[#value].size() ; i++)\
+	{\
+		value.push_back(jsonData[#value][i]); \
+	}\
+}
+
 /// Transform children 전용 매크로 함수..뭔가 애매하다..
 /// decltype(value)::value_type 으로 자료형을 가져온다.
 #define POSTDESERIALIZE_VECTOR_PTR(value)\
@@ -40,14 +49,5 @@
 	for(int i = 0; i < jsonData[std::string("__ID__") + #value].size() ; i++)\
 	{\
 		value.push_back(static_cast<decltype(value)::value_type>(PurahEngine::FileManager::GetInstance().GetAddress(jsonData[std::string("__ID__") + #value][i]))); \
-	}\
-}
-
-/// 범용적으로 벡터를 담을 매크로함수 만들기가 좀 어렵다. 더 고민해보는중.
-#define POSTDESERIALIZE_VECTOR(value)\
-{\
-	for(int i = 0; i < jsonData[#value].size() ; i++)\
-	{\
-		value.push_back(static_cast<decltype(value)::value_type>(PurahEngine::FileManager::GetInstance().GetAddress(jsonData[#value][i]))); \
 	}\
 }
