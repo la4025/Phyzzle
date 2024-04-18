9077ef1debfbb3cad148fa553f780117ae3c24ae
nadong <ehdgns4025@gmail.com>
2024-04-18 19:20:54 +0900

    이벤트함수 재 수정...제발.....이번에 성공하게 해주세요...

--- PurahEngine/GameObject.cpp
+++ PurahEngine/GameObject.cpp
@@ -138,7 +138,7 @@ void PurahEngine::GameObject::StartEvent(std::queue<std::pair<Component*, std::f
 		{
 			for (int i = 0; i < trans->GetChildren().size(); i++)
 			{
-				trans->GetChildren()[i]->GetGameObject()->AwakeEvent(eventQueue, false);
+				trans->GetChildren()[i]->GetGameObject()->StartEvent(eventQueue, false);
 			}
 		}
 
@@ -146,14 +146,14 @@ void PurahEngine::GameObject::StartEvent(std::queue<std::pair<Component*, std::f
 		{
 			for (int i = 0; i < trans->GetChildren().size(); i++)
 			{
-				trans->GetChildren()[i]->GetGameObject()->AwakeEvent(eventQueue, true);
+				trans->GetChildren()[i]->GetGameObject()->StartEvent(eventQueue, true);
 			}
 		}
 		else
 		{
 			for (int i = 0; i < trans->GetChildren().size(); i++)
 			{
-				trans->GetChildren()[i]->GetGameObject()->AwakeEvent(eventQueue, false);
+				trans->GetChildren()[i]->GetGameObject()->StartEvent(eventQueue, false);
 			}
 		}
 	}
