d9050106d554d5b5303c268c32754b0ad1691f3c
BEOMJOON KIM <tksabum@gmail.com>
2024-04-25 12:03:28 +0900

    RenderInfo, RenderInfoManager 클래스 추가 아직 구현하지 않음

---
+++ ZeldaGraphics/RenderInfo.h
@@ -0,0 +1,32 @@
+#pragma once
+
+enum class RenderType
+{
+	Deferred,
+	Forward
+};
+
+enum class DeferredType
+{
+	None
+};
+
+enum class PostProcessingType
+{
+	None,
+	FastOutLine,
+	OutLine
+};
+
+enum class ForwardType
+{
+	None,
+	WireFrame
+};
+
+class RenderInfo
+{
+public:
+
+};
+
---
+++ ZeldaGraphics/RenderInfoManager.cpp
@@ -0,0 +1,1 @@
+#include "RenderInfoManager.h"
